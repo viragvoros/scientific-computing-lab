4e93be6d59c040adacc9a01f6fb73fc16456f880
Radu Raicea <radu.raicea@tum.de>
2020-12-01 19:58:46 +0100

    Change style for all but main.m

--- Worksheet 2/odeHEUN.m
+++ Worksheet 2/odeHEUN.m
@@ -1,16 +1,35 @@
-% Heun method calculation
-function [t,p] = odeHEUN(ODE,t_init,t_last,delta_t,p_init)
-N = (t_last-t_init)/delta_t;                % Number of calculated points
-p = zeros(N,1);                             % Initializing p vector
-t = zeros(N,1);                             % Initializing time vector
-t(1) = 0;                                   % Initial value for time
-p(1) = p_init;                              % Initial value for p
-for i=1:N-1
-    t(i+1)=t(i) + delta_t;
-    Y1 = ODE(t(i), p(i));                   % y'(n)
-    Y2 = ODE(t(i)+delta_t, p(i)+Y1*delta_t);% y'(n+1)
+function [t, p] = odeHEUN(ODE, t_init, t_last, delta_t, p0)
+% odeHEUN uses Heun's Method to compute discrete points of the solution
+% function for the given ODE.
+%
+% Inputs:
+%   ODE     = Derivative function that takes the p value.
+%   t_init  = Initial t value from which to start stepping from.
+%   t_last  = Last t value where to stop stepping.
+%   delta_t = Size of the time step.
+%   p0      = Initial value (at t0 = 0).
+%
+% Outputs:
+%   [t, p]  = Vector containing the approximated values of p for a given t.
+
+% Number of calculated points
+N = (t_last - t_init) / delta_t;
+
+% Initialize t and p vectors with the initial condition
+t = zeros(N, 1);
+p = zeros(N, 1);
+t(1) = 0;
+p(1) = p0;
+
+for i = 1:N - 1
+    t(i+1) = t(i) + delta_t;
     
-    % Definition: y(n+1)=y(n)+0.5(y'(n)+y'(n+1))Δt
-    p(i+1)=p(i) + (delta_t/2)*(Y1+Y2);     
+    % Calculate intermediate values
+    y_n = ODE(p(i));
+    y_n_plus_1 = ODE(p(i) + y_n * delta_t);
+    
+    % Definition: y(n+1) = y(n) + 0.5 * Δt * (y'(n) + y'(n+1))
+    p(i+1) = p(i) + 0.5 * delta_t * (y_n + y_n_plus_1);
 end
+
 end
--- Worksheet 2/odeRUNGE.m
+++ Worksheet 2/odeRUNGE.m
@@ -1,18 +1,36 @@
-% Runge-Kutta method calculation
-function [t,p] = odeRUNGE(ODE,t_init,t_last,delta_t,p_init)
-N = (t_last-t_init)/delta_t;               % Number of calculated points
-p = zeros(N,1);                            % Initializing p vector
-t = zeros(N,1);                            % Initializing time vector
-t(1) = 0;                                  % Initial value for time
-p(1) = p_init;                             % Initial value for p
-for i=1:N-1
-    t(i+1)=t(i) + delta_t;
-    Y1 = ODE(t(i), p(i));
-    Y2 = ODE(t(i)+0.5*delta_t,p(i)+0.5*Y1*delta_t);
-    Y3 = ODE(t(i)+0.5*delta_t,p(i)+0.5*Y2*delta_t);
-    Y4 = ODE(t(i)+delta_t,p(i)+Y3*delta_t);
+function [t, p] = odeRUNGE(ODE, t_init, t_last, delta_t, p0)
+% odeRUNGE uses the Fourth Order Runge-Kutta Method (RK4) to compute
+% discrete points of the solution function for the given ODE.
+%
+% Inputs:
+%   ODE     = Derivative function that takes the p value.
+%   t_init  = Initial t value from which to start stepping from.
+%   t_last  = Last t value where to stop stepping.
+%   delta_t = Size of the time step.
+%   p0      = Initial value (at t0 = 0).
+%
+% Outputs:
+%   [t, p]  = Vector containing the approximated values of p for a given t.
+
+% Number of calculated points
+N = (t_last - t_init) / delta_t;
+
+% Initialize t and p vectors with the initial condition
+t = zeros(N, 1);
+p = zeros(N, 1);
+t(1) = 0;
+p(1) = p0;
+
+for i = 1:N - 1
+    t(i+1) = t(i) + delta_t;
     
-    % Definition: y(n+1)=y(n)+1/6(Y1+2(Y2)+2(Y3)+Y4)Δt
-    p(i+1) = p(i)+((Y1+2*Y2+2*Y3+Y4)/6)*delta_t;
+    k1 = ODE(p(i));
+    k2 = ODE(p(i) + 0.5 * k1 * delta_t);
+    k3 = ODE(p(i) + 0.5 * k2 * delta_t);
+    k4 = ODE(p(i) + k3 * delta_t);
+    
+    % Definition: y(n+1) = y(n) + (1/6)(k1 + 2(k2) + 2(k3) + k4) * Δt
+    p(i+1) = p(i) + ((k1 + 2*k2 + 2*k3 + k4) / 6) * delta_t;
 end
+
 end
