fa699dac904d02b924f2863761b919e98c02033d
Radu Raicea <radu.raicea@tum.de>
2021-01-31 15:33:16 +0100

    Save images

--- Worksheet 5/worksheet5.m
+++ Worksheet 5/worksheet5.m
@@ -4,6 +4,11 @@ clear all
 close all
 clc
 
+% Flags to enable or disable the displaying of graphs, and the saving of
+% images.
+display_graphs = true;
+save_images = true;
+
 L = 1;                      % domain length along x and y
 n = [3, 7, 15, 31];         % number of grid points along x and y
 tolerance = 1e-6;           % convergence criterion
@@ -70,38 +75,6 @@ for m = 1:length(n)
     end
 end
 
-figure(length(final_t)+1)
-sgtitle_text = sprintf(['Implicitly using Gauss-Seidel δt = %g'], dt(1));
-sgtitle(sgtitle_text);
-
-for m = 1:length(n)
-    for t = 1:length(final_t)
-        subplot(length(n), length(final_t), t+(m-1)*length(final_t))
-        surf(T_states{m, length(dt)+1, t, 1}, T_states{m, length(dt)+1, t, 2}, T_states{m, length(dt)+1, t, 3});
-        xlabel('X axis');
-        ylabel('Y axis');
-        title_text_imp = sprintf(['Nx = Ny = %d \nt = %g'], n(m), final_t(t));
-        title(title_text_imp);
-    end
-end
-
-for t = 1:length(final_t)
-    figure(t)
-    sgtitle_text_exp = sprintf(['Explicitly at t = %g'], final_t(t));
-    sgtitle(sgtitle_text_exp);
-    
-    for m = 1:length(n)
-        for l = 1:length(dt)
-            subplot(length(n), length(dt), l+(m-1)*length(dt))
-            surf(T_states{m, l, t, 1}, T_states{m, l, t, 2}, T_states{m, l, t, 3});
-            xlabel('X axis');
-            ylabel('Y axis');
-            title_text_exp = sprintf(['Nx = Ny = %d \nδt = %g'], n(m), dt(l));
-            title(title_text_exp);
-        end
-    end
-end
-
 for t = 1:length(final_t)
     table_stable_cases = array2table(...
         stability(:,:,t)', ...
@@ -124,4 +97,90 @@ for t = 1:length(final_t)
     fprintf(['\n\n\nStable cases explicitly at t = %g\n\n'], final_t(t));
     disp(table_stable_cases);
 end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Plotting and display the graphs. This is enabled by the `display_graphs`
+% flag.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if display_graphs
+    for t = 1:length(final_t)
+        figure(t);
+        set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
+        sgtitle_text_exp = sprintf(['Explicitly at t = %g'], final_t(t));
+        sgtitle(sgtitle_text_exp);
+        
+        for m = 1:length(n)
+            for l = 1:length(dt)
+                subplot(length(n), length(dt), l+(m-1)*length(dt))
+                surf(T_states{m, l, t, 1}, T_states{m, l, t, 2}, T_states{m, l, t, 3});0
+                xlabel('X');
+                ylabel('Y');
+                zlabel('T');
+                title_text_exp = sprintf(['Nx = Ny = %d \nδt = %g'], n(m), dt(l));
+                title(title_text_exp);
+            end
+        end
+    end
+    
+    figure(length(final_t)+1);
+    set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
+    sgtitle_text = sprintf(['Implicitly using Gauss-Seidel δt = %g'], dt(1));
+    sgtitle(sgtitle_text);
+    
+    for m = 1:length(n)
+        for t = 1:length(final_t)
+            subplot(length(n), length(final_t), t+(m-1)*length(final_t))
+            surf(T_states{m, length(dt)+1, t, 1}, T_states{m, length(dt)+1, t, 2}, T_states{m, length(dt)+1, t, 3});
+            xlabel('X');
+            ylabel('Y');
+            zlabel('T');
+            title_text_imp = sprintf(['Nx = Ny = %d \nt = %g'], n(m), final_t(t));
+            title(title_text_imp);
+        end
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Saving images of each plot in the output/ directory. This is enabled by
+% the `save_images` flag. This will add around a minute or two to the
+% execution time.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if save_images
+    mkdir('output');
+    saving_figure = figure('visible', 'off');
+    
+
+    for t = 1:length(final_t)
+        for m = 1:length(n)
+            for l = 1:length(dt)
+                set(0, 'CurrentFigure', saving_figure);
+                surf(T_states{m, l, t, 1}, T_states{m, l, t, 2}, T_states{m, l, t, 3});
+                xlabel('X');
+                ylabel('Y');
+                zlabel('T');
+                sgtitle_text_exp = sprintf(['Explicitly at t = %g\nNx = Ny = %d \nδt = %g'], final_t(t), n(m), dt(l));
+                sgtitle(sgtitle_text_exp);
+                exportgraphics(saving_figure, sprintf(['output/mesh_%d_dt_%g_final_t_%g.png'], n(m), dt(l), final_t(t)));
+            end
+        end
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
